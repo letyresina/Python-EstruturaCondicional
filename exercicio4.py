fb53152542d058c01bfc20dca354c961d97c235a
letyresinaa <leticiaresina@gmail.com>
2023-03-28 08:46:06 -0300

    Subindo exercício 4

---
+++ exercicio4.py
@@ -0,0 +1,10 @@
+# Exercício 4: Descobrir se um caractere é vogal ou consoante
+
+# O .lower() transforma todos os caracteres em minúsculos para facilitar
+letra = input("Informe uma letra qualquer: ").lower()
+
+if (letra == "a" or letra == "e" or letra == "i" or 
+    letra == "o" or letra == "u" ): 
+        print(f"A letra é uma vogal")
+else:
+    print(f"A letra é uma consoante")
