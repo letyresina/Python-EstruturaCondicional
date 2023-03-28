3593e7fe782c2677ff1c3f3f1ddd721f147345a6
letyresinaa <leticiaresina@gmail.com>
2023-03-28 09:26:52 -0300

    Subindo exexercício 8

---
+++ exercicio8.py
@@ -0,0 +1,14 @@
+# Exercício 8: Solicita 3 números e exibe na tela o menor deles, e se for igual, mostra a mensagem de "Números iguais"
+
+num1 = float(input("Informe um número qualquer: "))
+num2 = float(input("Informe um outro número qualquer: "))
+num3 = float(input("Informe um outro número qualquer: "))
+
+if (num1 < num2 and num1 < num3):
+    print(f"O menor número é {num1}")
+elif(num2 < num1 and num2 < num3):
+    print(f"O menor número é {num2}")
+elif(num3 < num1 and num3 < num2):
+    print(f"O menor número é {num3}")
+else:
+    print("Números iguais")
