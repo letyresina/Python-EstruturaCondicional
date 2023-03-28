bab99d35b9c02c1beab3859017338b9f46bfbe79
letyresinaa <leticiaresina@gmail.com>
2023-03-28 09:40:48 -0300

    Pequeno ajuste feito no código

--- exercicio9.py
+++ exercicio9.py
@@ -6,7 +6,7 @@ lado3 = float(input("Informe o terceiro lado do triângulo: "))
 
 if (lado1 == lado2 and lado1 == lado3 and lado2 == lado3):
     print(f"O triângulo é equilátero.")
-if (lado1 == lado2 and lado1 != lado3) or (lado2 == lado3 and lado2 != lado1) or (lado3 == lado1 and lado3 != lado1) or (lado1 == lado3 and lado1 != lado2):
+elif (lado1 == lado2 and lado1 != lado3) or (lado2 == lado3 and lado2 != lado1) or (lado3 == lado1 and lado3 != lado1) or (lado1 == lado3 and lado1 != lado2):
     print(f"O triângulo é isóceles.")
 else:
     print(f"O triângulo é escaleno.")
