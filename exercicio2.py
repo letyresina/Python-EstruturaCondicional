969cf280e209979e70b329555d668b657658afde
letyresinaa <leticiaresina@gmail.com>
2023-03-28 08:24:37 -0300

    Subindo o exercício 2

---
+++ exercicio2.py
@@ -0,0 +1,8 @@
+# Exercício 2: Informa se o número é par ou ímpar 
+
+numero = int(input("Informe um número do tipo inteiro: "))
+
+if (numero % 2 == 0):
+    print(f"O número {numero} é par")
+else:
+    print(f"O número {numero} é ímpar")
