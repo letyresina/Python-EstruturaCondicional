4f366b2066d75233da07021af85b161d2d3b424d
Leticia Resina <80417466+letyresina@users.noreply.github.com>
2023-03-31 11:05:06 -0300

    Comentário no exercício 4 com outra forma de resolução

--- exercicio4.py
+++ exercicio4.py
@@ -8,3 +8,12 @@ if (letra == "a" or letra == "e" or letra == "i" or
         print(f"A letra é uma vogal")
 else:
     print(f"A letra é uma consoante")
+
+    '''
+        Você também pode fazer com
+
+        vogais = "aeiouAEIOU"
+
+        if letra in vogais:
+            print(f"A letra é uma vogal")
+    '''
